f9b6254c220c2218e3d9859eb94e0c1a4f07ab1c
Angell50 <angel.vela@udla.edu.ec>
2024-10-28 06:00:12 -0500

    cambios modelos  1/10/2024

---
+++ VelaAngel-ProyectoMVC/Models/Carrera.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VelaAngel_ProyectoMVC.Models
+{
+    public class Carrera
+    {
+        [Required]
+        [Range(1,1000)]
+        public string Id { get; set; }
+
+        [AllowedValues("Software","Redes")]
+        public string Nombre { get; set; }
+    }
+}
---
+++ VelaAngel-ProyectoMVC/Models/EstudianteUDLA.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace VelaAngel_ProyectoMVC.Models
+{
+    public class EstudianteUDLA
+    {
+        [Required]
+        public string IDBanner { get; set; }
+
+        [AllowNull]
+        public string Correo { get; set; }
+
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        public Carrera Carrera { get; set; }
+    }
+      
+}
